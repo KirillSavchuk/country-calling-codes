f7f1170ea4b44c16b7818c39c088e1b4354ffacb
Kirill Savchuk <ksavcuks@eisgroup.com>
2022-10-07 18:44:13 +0300

    Initialized backend Application with all pom.xml dependencies

---
+++ backend/src/main/java/lv/savchuk/country/calling/codes/CountryCallingCodeApplication.java
@@ -0,0 +1,13 @@
+package lv.savchuk.country.calling.codes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CountryCallingCodeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CountryCallingCodeApplication.class, args);
+    }
+
+}
